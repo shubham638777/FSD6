e021d179e30bf36232a0eac9f52db68d207e79cf
shubham638777 <shubhamsingh26067@gmail.com>
2026-02-09 12:38:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const getData = async () => {
+    try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const res = await response.json()
+        console.log(res);
+    } catch (error) {
+      console.log("unable to fetch data");      
+    }
+}
+getData();
+
+// const myPromise = new Promise((resolve, reject) => {
+//     let marks = 80;
+//     if (marks > 33) {
+//         resolve("pass");
+//     } else {
+//         reject("fail");
+//     }
+// })
+// const f1 = async () => {
+//     const msg = await myPromise;
+//     console.log(msg);
+// }
+// // f1();
+// // myPromise.then((msg) =>console.log(msg))
+// //     .catch((error)=>console.log("some error occured."))
+// // // console.log(myPromise);
